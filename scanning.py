45863fa0089a688e8db5c74c1d8284b8e14e5fa5
Savva Spiridonov <savvasapir@gmail.com>
2023-11-16 00:51:00 +0200

    feat[server]: create get interface and subnetmask info

--- scanning.py
+++ scanning.py
@@ -1,6 +1,6 @@
 import scapy.all as scapy
-
-INTERFACE = "Ethernet"
+import ipaddress
+import netifaces
 
 class Device():
     def __init__(self, ip:str, name:str, mac:str, is_available:bool):
@@ -12,13 +12,33 @@ class Device():
     def is_active(self) -> bool:
         return self.is_available
 
+def get_interface_name(interface_guid):
+    l = scapy.get_if_list()
+    ifaces = scapy.IFACES.data
+    my_iface = next((x for x in l if ifaces[x].guid == interface_guid), None)
+    return ifaces[my_iface].name
+
+def get_subnet_mask():
+    try:
+        default_interface = netifaces.gateways()['default'][netifaces.AF_INET][1]
+        global interface_name 
+        interface_name = get_interface_name(default_interface)
+
+        if_info = netifaces.ifaddresses(default_interface)[netifaces.AF_INET][0]            # Get the interface's IPv4 address and netmask
+        ip_address = if_info['addr']
+        subnet_mask = if_info['netmask']
+
+        return f"{ip_address}/{subnet_mask}"
+    except Exception as e:
+        print(f"Error getting subnet mask: {e}")
+        return None
 
 def send_arp(ip):
     arp_request = scapy.ARP(pdst = ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 
     try:
-        ans, unans = scapy.srp(broadcast/arp_request, iface = INTERFACE, timeout=0.1, verbose=False)
+        ans, unans = scapy.srp(broadcast/arp_request, iface = interface_name, timeout=0.1, verbose=False)
         mac = ans[0][1].hwsrc
     except Exception:
         return None
