097f47b43ccfc37dc09c0356d18f76b184a2a961
Amir Palmeri <amirpalmeri@gmail.com>
2023-11-17 14:55:53 +0200

    fix: output list of devices ips

--- Second_Mapping.py
+++ Second_Mapping.py
@@ -1,4 +1,4 @@
-from scapy.all import sr1, IP, ICMP
+from scapy.all import sr, IP, ICMP
 from ipaddress import IPv4Network
 import netifaces
 import subprocess
@@ -9,42 +9,37 @@ def get_subnet_mask():
         # Get the default network interface
         default_interface = netifaces.gateways()['default'][netifaces.AF_INET][1]
 
-        # Get the interface's IPv4 address and netmask
         if_info = netifaces.ifaddresses(default_interface)[netifaces.AF_INET][0]
         ip_address = if_info['addr']
         subnet_mask = if_info['netmask']
-
-        broadcast = if_info['broadcast']
-
-        return f"{ip_address}/{subnet_mask}", broadcast
+        return f"{ip_address}/{subnet_mask}"
+    
     except Exception as e:
         print(f"Error getting subnet mask: {e}")
         return None
 
 
-def discover_devices(subnet, broadcast):
+def discover_devices(subnet):
     devices = []
 
     # Iterate over all IP addresses in the subnet
+    for ip in IPv4Network(subnet, strict=False).hosts():
+        ip = str(ip)
+        packet = IP(dst=ip)/ICMP()
+        response, _ = sr(packet, timeout=0.2, verbose=0)
 
-    # for ip in IPv4Network(subnet, strict=False).hosts():
-    ip = str(broadcast)
-    
-    packet = IP(dst=ip)/ICMP()
-    response = sr1(packet, timeout=1, verbose=0)
-
-    if response and response.haslayer(ICMP) and response[ICMP].type == 0:
-        print(ip)
-        devices.append(ip)
+        for sent_packet, received_packet in response:
+            if received_packet.haslayer(ICMP) and received_packet[ICMP].type == 0:
+                devices.append(ip)
 
     return devices
 
 def main():
-    subnet, broadcast = get_subnet_mask()
+    subnet = get_subnet_mask()
 
     print(f"Scanning devices in subnet: {subnet}")
 
-    devices = discover_devices(subnet, broadcast)
+    devices = discover_devices(subnet)
 
     if devices:
         print("Devices discovered:")
