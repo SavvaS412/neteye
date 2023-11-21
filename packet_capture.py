63e8e599daefc81a46c6fc7a24b9ab09a48f2ebc
Savva Spiridonov <savvasapir@gmail.com>
2023-11-22 01:24:49 +0200

    feat: add gathering correct interface

--- packet_capture.py
+++ packet_capture.py
@@ -1,4 +1,26 @@
 import scapy.all as scapy
+import netifaces
+
+def get_interface_name(interface_guid):
+    l = scapy.get_if_list()
+    ifaces = scapy.IFACES.data
+    my_iface = next((x for x in l if ifaces[x].guid == interface_guid), None)
+    return ifaces[my_iface].name
+
+def get_ip():
+    try:
+        default_interface = netifaces.gateways()['default'][netifaces.AF_INET][1]
+        global interface_name 
+        interface_name = get_interface_name(default_interface)
+
+        if_info = netifaces.ifaddresses(default_interface)[netifaces.AF_INET][0]            # Get the interface's IPv4 address and netmask
+        ip_address = if_info['addr']
+
+        return f"{ip_address}"
+    except Exception as e:
+        print(f"Error getting subnet mask: {e}")
+        return None
+
 def print_packet(packet):
     print(packet.summary())
 
