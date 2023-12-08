781a245271a47a330839d07deea371dcf3315fe6
Savva Spiridonov <savvasapir@gmail.com>
2023-12-08 09:54:28 +0200

    fix: action error handling

--- detection.py
+++ detection.py
@@ -8,9 +8,9 @@ class Action(Enum):
     GREATER = 1
     GREATER_EQUAL = 2
 
-def check_statement(parameter, action, amount):
+def check_statement(parameter : int, action : int, amount : int):
     if Action(action) == Action.LESS_EQUAL:
-            statement = parameter <= amount
+        statement = parameter <= amount
 
     elif Action(action) == Action.LESS:
         statement = parameter < amount
@@ -24,24 +24,21 @@ def check_statement(parameter, action, amount):
     elif Action(action) == Action.GREATER_EQUAL:
         statement = parameter >= amount
 
-    else:
-        return None
-
     return statement 
 
 def detect_rules(rules : list[Rule]):
     for rule in rules:
-        statement = check_statement(rule.parameter, rule.action, rule.amount)
+        try:
+            statement = check_statement(rule.parameter, rule.action, rule.amount)
+        except ValueError as e:
+            print(f"ERR: unknown rule action in rule '{rule.name}' -", e)
+            statement = False
 
         if statement:
             print("Notification:", rule.name)
 
-        elif not statement:
+        else:
             print("No Notification:", rule.name)
 
-        else:
-            print("ERR: unknown rule action", rule.name, rule.action)
-
-
 if __name__ == '__main__':
-    detect_rules([Rule("latencytest", 1, 150, 100), Rule("latencytest2", 1, 15, 100)])
+    detect_rules([Rule("test greater", 1, 150, 100), Rule("test wrong", 1, 15, 100), Rule("test equal", 0, 15, 15), Rule("test less", -1, 15, 100), Rule("test less wrong", -1, 15, 15), Rule("test less equal", -2, 15, 15), Rule("test error", 3, 15, 100)])
