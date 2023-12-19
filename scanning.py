3bef64f553461d2d4d03fc445aaeb3bdaf699b0f
Savva Spiridonov <savvasapir@gmail.com>
2023-12-19 20:35:02 +0200

    fix: merge errors

--- scanning.py
+++ scanning.py
@@ -184,12 +184,7 @@ def scan_update(device_list):
         for device in device_list:
             device_details = send_ping(device.ip, 1)
             if device_details:
-                check_name_and_latency(device_list, device, device_details)
-            else:
-                device_details = send_ping(device.ip, 3) # check maybe sleep instead
-                if device_details:
-                    check_name_and_latency(device_list, device, device_details)
-                else:
+                if device:
                     device_list.remove(device)
                     if device.name != device_details['name']:
                         device.name = device_details['name']
@@ -201,9 +196,9 @@ def scan_update(device_list):
                         
                     device_list.append(device)
             elif device:
-                    device_list.remove(device)
-                    if debug:
-                        print("removed", device.ip)
+                device_list.remove(device)
+                if debug:
+                    print("removed", device.ip)
 
     except Exception as e:
         print(f"Error updating scan: {e}")
