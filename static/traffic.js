6874d2b60c49a3c519987f1f8cd7eafee1888705
Savva Spiridonov <savvasapir@gmail.com>
2024-03-27 20:32:18 +0200

    feat: fetch traffic from server

--- static/traffic.js
+++ static/traffic.js
@@ -1,7 +1,43 @@
 
+function getPacketHTML(packet) {
+    let layersHTML = `<div class="details">`
+    for (const layerName in packet.layers) {
+        if (Object.hasOwnProperty.call(packet.layers, layerName)) {
+            const layer = packet.layers[layerName];
+            let HTML = `
+                <div class="layer">
+                <h3>${layerName}</h3>
+            `;
+            for (const key in layer) {
+                if (Object.hasOwnProperty.call(layer, key)) {
+                    const value = layer[key];
+                    HTML += `<p><b>${key}:</b> ${value}</p>`;
+                }
+            }
+            HTML += `</div>`
+            layersHTML += HTML;
+        }
+    }
+    layersHTML += `</div>`;
+
+    const template = `
+    <div class="title">
+        <span class="arrow">▶</span>
+        <h2 class="summary">${packet.summary}</h2>
+    </div>
+    ${layersHTML}
+    `;
+    return template
+}
 
 function addPacket(packet) {
-    console.log(packet);
+    let packetList = document.getElementById("packet-list");
+    let packetItem = document.createElement("li");
+    packetItem.classList.add("packet");
+    packetItem.innerHTML = getPacketHTML(packet);
+    packetItem.addEventListener('click', function() { this.classList.toggle("active"); }, false);
+
+    packetList.insertBefore(packetItem, packetList.firstChild);
 }
 
 function fetchPackets() {
@@ -25,12 +61,12 @@ function fetchPackets() {
         }
     })
     .catch(error => {
-        console.error("Could't fetch from '/api/notifications':", error);
+        console.error("Couldn't fetch from '/api/traffic':", error);
     });
 }
 
 window.onload = function () {
     fetchPackets();
-    const updateInterval = 5 * 1000      //TODO: change this to take out of settings
+    const updateInterval = 3 * 1000      //TODO: change this to take out of settings
     setInterval(fetchPackets, updateInterval);
 }
